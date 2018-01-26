709a0f0f9e445c2183fb61a8e1b9acee957b4586
Mohammad Rizky Alimansyah <mohammadrizkyalimansyah@gmail.com>
2018-01-26 13:16:20 +0700

    initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public Transform stepObj;
+    public int stepDistance = 5;
+    public int maxStep = 10;
+
+    public float currentTime = 10;
+    public bool timeOn = true;
+    public Image bar_time;
+    public Text text_time, text_score, text_step;
+
+
+    // Use this for initialization
+    void Start () {
+        /*for (int i = 0; i < maxStep; i++)
+        {
+            Instantiate(stepObj, new Vector3(i * stepDistance, -2, 0), Quaternion.identity);
+            Debug.Log(stepObj.transform.position);
+
+        }*/
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (timeOn)
+        {
+            if (currentTime >= 0)
+            {
+                currentTime -= Time.deltaTime;
+                text_time.text = System.Math.Round(currentTime).ToString();
+                bar_time.fillAmount -= Time.deltaTime * 0.1f;
+
+            }
+            else
+            {
+                Debug.Log("Time's Up");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+    public Transform target;
+    public float jumpSpeed;
+    bool active = true;
+
+    //public GameObject[] step;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (active)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, jumpSpeed * Time.deltaTime);
+        }
+    }
+}
