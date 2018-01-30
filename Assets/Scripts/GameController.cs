aeffbc7030c936a4f2616c5b0bbbec8ee93826d4
Mohammad Rizky Alimansyah <mohammadrizkyalimansyah@gmail.com>
2018-01-31 05:13:20 +0700

    update pergerakan ikan

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,18 +4,24 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
     public Transform stepObj;
-    public int stepDistance = 5;
-    public int maxStep = 10;
+    public Transform[] fish;
+    public Transform[] target;
+    //public int stepDistance = 5;
+    //public int maxStep = 10;
 
     public float currentTime = 10;
+    public float speed;
     public bool timeOn = true;
     public Image bar_time;
     public Text text_time, text_score, text_step;
 
+    private int current;
 
     // Use this for initialization
     void Start () {
-        /*for (int i = 0; i < maxStep; i++)
+
+        /*Instantiate Step
+        for (int i = 0; i < maxStep; i++)
         {
             Instantiate(stepObj, new Vector3(i * stepDistance, -2, 0), Quaternion.identity);
             Debug.Log(stepObj.transform.position);
@@ -25,6 +31,15 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        //fish movement
+        if (fish[0].transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(fish[0].transform.position, target[current].position, speed * Time.deltaTime);
+            fish[0].GetComponent<Rigidbody2D>().MovePosition(pos);
+        }
+
+        //time bar update
         if (timeOn)
         {
             if (currentTime >= 0)
---
+++ Assets/Scripts/MoveFish.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFish : MonoBehaviour {
+
+    public Transform[] target;
+    public float speed;
+    private int current;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //fish movement
+        if (transform.position != target[current].position)
+        {
+            
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody2D>().MovePosition(pos);
+            print("ikan harusnya gerak");
+        }
+        else current = (current + 1) % target.Length;
+    }
+}
